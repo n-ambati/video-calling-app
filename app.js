a7f4309ce99e34e9a6befd50da4d0e4fa7242e0f
naveen <ambatinaveen24@gmail.com>
2021-08-18 21:55:37 +0530

    Updated indentation to 2 spaces

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 async function getConnectedDevices(type) {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    return devices.filter(device => device.kind === type);
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  return devices.filter(device => device.kind === type);
 }
 
 async function openCamera(cameraId, minWidth, minHeight) {
-    const constraints = {
-        'video': {
-            'deviceId': cameraId,
-            'width': { 'min': minWidth },
-            'height': { 'min': minHeight },
-        },
-        'audio': {
-            'echoCancellation': true,
-        },
-    }
+  const constraints = {
+    'video': {
+      'deviceId': cameraId,
+      'width': { 'min': minWidth },
+      'height': { 'min': minHeight },
+    },
+    'audio': {
+      'echoCancellation': true,
+    },
+  }
 
-    return await navigator.mediaDevices.getUserMedia(constraints);
+  return await navigator.mediaDevices.getUserMedia(constraints);
 }
 
 const videoElement = document.querySelector('video#localVideo');
@@ -26,10 +26,10 @@ const videoElement = document.querySelector('video#localVideo');
 
 
 (async function() {
-    const cameras = await getConnectedDevices('videoinput');
+  const cameras = await getConnectedDevices('videoinput');
 
-    if (cameras && cameras.length > 0) {
-        const stream = await openCamera(cameras[0].deviceId, 1280, 720);
-        videoElement.srcObject = stream;
-    }
+  if (cameras && cameras.length > 0) {
+    const stream = await openCamera(cameras[0].deviceId, 1280, 720);
+    videoElement.srcObject = stream;
+  }
 })();
